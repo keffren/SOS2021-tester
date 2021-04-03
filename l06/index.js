9c52f4f1f0bb330678fadc5997340824459a87d0
keffren <matsergir1@alum.us.es>
2021-04-03 14:45:15 +0200

    datos incorporados

--- l06/index.js
+++ l06/index.js
@@ -72,6 +72,20 @@ var r_hostelries = [
         "employee_staff"    :   147190,
         "establishment_open":   14751,
         "traveler_numer"    :   5787656
+    },
+    {
+        "district"           :   "Catalonia",
+        "year"              :   "2019",
+        "employee_staff"    :   418558,
+        "establishment_open":   27722,
+        "traveler_numer"    :   20752391
+    },
+    {
+        "district"           :   "Catalonia",
+        "year"              :   "2018",
+        "employee_staff"    :   400203,
+        "establishment_open":   27213,
+        "traveler_numer"    :   20045332
     }
 
 ];
